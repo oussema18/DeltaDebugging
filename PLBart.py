f500545c813637b034073c5cd0b65307ea12f67b
oussema18 <gib.masmoudi@gmail.com>
2024-03-13 00:39:30 +0100

    chnages

--- PLBart.py
+++ PLBart.py
@@ -1,26 +1,40 @@
-from transformers import PLBartForConditionalGeneration, PLBartTokenizer
+# Example reference and candidate sentences
+from smooth_bleu import bleu
+import pandas as pd
 
 
-def replace_line_breaks(code):
-    # Replace line breaks with "NEW_LINE_INDENT"
-    code_with_new_line_indent = code.replace("\n", "NEW_LINE_INDENT")
-
-    # Handle the indentation for the first line
-    if code_with_new_line_indent.startswith("NEW_LINE_INDENT"):
-        code_with_new_line_indent = code_with_new_line_indent[len("NEW_LINE_INDENT") :]
-
-    return code_with_new_line_indent
+def calculate_bleu_for_row(reference_sentence, candidate_sentence):
+    # Calculate the smoothed BLEU score
+    bleu_score = bleu(
+        [reference_sentence], candidate_sentence, smooth=1
+    )  # Enable smoothing
+    return bleu_score[0] * 100
 
 
-tokenizer = PLBartTokenizer.from_pretrained(
-    "uclanlp/plbart-python-en_XX", src_lang="python", tgt_lang="en_XX"
+# Load the Excel file
+file_path = (
+    "C:/Users/oussama/Desktop/Bachelor_Thesis/Results_1000_functions_PLBART.xlsx"
 )
+df = pd.read_excel(file_path, sheet_name="Without_Comments", engine="openpyxl")
 
-example_python_phrase = """"vidfromurl("Extracts video IDURL."""
-inputs = tokenizer(example_python_phrase, return_tensors="pt")
-model = PLBartForConditionalGeneration.from_pretrained("uclanlp/plbart-python-en_XX")
+# Ensure the BLEU scores list is empty
+bleu_scores = []
 
-translated_tokens = model.generate(
-    **inputs, decoder_start_token_id=tokenizer.lang_code_to_id["__en_XX__"]
+# Iterate over each row in the DataFrame
+for index, row in df.iterrows():
+    reference = row["Comments"]
+    candidate = row["Original Prediction"]
+    bleu_score = calculate_bleu_for_row(reference, candidate)
+    bleu_scores.append(bleu_score)
+
+# Add the BLEU scores as a new column to the DataFrame
+df["Smoothed BLEU"] = bleu_scores
+
+# Write the modified DataFrame back to an Excel file
+output_file_path = (
+    "C:/Users/oussama/Desktop/Bachelor_Thesis/Results_1000_functions_PLBART.xlsx"
 )
-print(tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0])
+with pd.ExcelWriter(
+    output_file_path, engine="openpyxl", mode="a", if_sheet_exists="new"
+) as writer:
+    df.to_excel(writer, sheet_name="BLEU Scores", index=False)
