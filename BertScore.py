985c4b58f5ab38ac3c7a0df725666c182adb8b9c
oussema18 <gib.masmoudi@gmail.com>
2024-03-13 00:50:31 +0100

    fix

--- BertScore.py
+++ BertScore.py
@@ -1,32 +1,15 @@
-from transformers import AutoTokenizer, T5ForConditionalGeneration
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-# Load the tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("Salesforce/codeT5-base")
-model = T5ForConditionalGeneration.from_pretrained("Salesforce/codeT5-base")
-
-# Encode the input text
-input_text = "your code snippet here"
-input_dict = tokenizer(
-    input_text,
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
+input_ids = tokenizer(
+    "summarize: studies have shown that owning a dog is good for you ",
     return_tensors="pt",
-    padding="max_length",
-    truncation=True,
-    max_length=512,
+).input_ids
+decoder_input_ids = tokenizer(
+    "<pad>", add_special_tokens=False, return_tensors="pt"
+).input_ids
+model = T5ForConditionalGeneration.from_pretrained("t5-small", return_dict=True)
+outputs = model(
+    input_ids=input_ids, decoder_input_ids=decoder_input_ids, output_attentions=True
 )
-
-# For T5, decoder_input_ids are often created by shifting the input_ids to the right and adding the pad token at the beginning
-decoder_start_token_id = model.config.decoder_start_token_id
-input_dict["decoder_input_ids"] = model._shift_right(input_dict["input_ids"])
-
-# Run the model and get the attention scores
-outputs = model(**input_dict, output_attentions=True)
-
-# Check if 'attentions' is in the outputs
-if "attentions" in outputs:
-    attentions = outputs.attentions
-else:
-    raise ValueError(
-        "No attentions were found. Ensure that `output_attentions=True` is set."
-    )
-
-# If attentions are found, you can proceed to process them
+print(outputs.encoder_attentions)
