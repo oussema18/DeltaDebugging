da3b55075d065719529ab8c3460e2be3140e87cd
oussema18 <gib.masmoudi@gmail.com>
2024-03-13 00:56:13 +0100

    fix

--- BertScore.py
+++ BertScore.py
@@ -1,14 +1,14 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-tokenizer = T5Tokenizer.from_pretrained("t5-small")
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/codeT5-small")
+model = AutoModelForSeq2SeqLM.from_pretrained("Salesforce/codeT5-small")
 input_ids = tokenizer(
-    "summarize: studies have shown that owning a dog is good for you ",
+    "def get_vid_from_url(url):\n        \"\"\"Extracts video ID from URL.\n        \"\"\"\n        return match1(url, r'youtu\\.be/([^?/]+)') or \\\n          match1(url, r'youtube\\.com/embed/([^/?]+)') or \\\n          match1(url, r'youtube\\.com/v/([^/?]+)') or \\\n          match1(url, r'youtube\\.com/watch/([^/?]+)') or \\\n          parse_query_param(url, 'v') or \\\n          parse_query_param(parse_query_param(url, 'u'), 'v')",
     return_tensors="pt",
 ).input_ids
 decoder_input_ids = tokenizer(
     "<pad>", add_special_tokens=False, return_tensors="pt"
 ).input_ids
-model = T5ForConditionalGeneration.from_pretrained("t5-small", return_dict=True)
 outputs = model(
     input_ids=input_ids, decoder_input_ids=decoder_input_ids, output_attentions=True
 )
